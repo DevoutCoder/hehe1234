aa2d4f9ed3a29b0e20b3456e601f0b990846c3b9
xiaoli <xiaoli@douniwan.com>
2015-09-26 17:37:47 +0800

    cvdfg g

--- hello/hello/ViewController.m
+++ hello/hello/ViewController.m
@@ -16,7 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    @"111111";
+    
 }
 
 - (void)didReceiveMemoryWarning {
